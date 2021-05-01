bbd82e62cd27558531caf04465b2eaddf9e26396
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2021-05-01 21:36:18 +0200

    add makerr

---
+++ makerr/caller_test.go
@@ -0,0 +1,27 @@
+package makerr_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetCaller_exact(t *testing.T) {
+	t.Parallel()
+
+	result := returnsExactLocation()
+
+	assert.Contains(t, result, "github.com/dc0d/wrapperr/makerr_test.returnsExactLocation")
+	assert.Contains(t, result, "makerr/fixtures_test.go")
+	assert.Contains(t, result, ":10")
+}
+
+func Test_GetCaller_caller(t *testing.T) {
+	t.Parallel()
+
+	result := returnsCallerLocation()
+
+	assert.Contains(t, result, "github.com/dc0d/wrapperr/makerr_test.returnsCallerLocation")
+	assert.Contains(t, result, "makerr/fixtures_test.go")
+	assert.Contains(t, result, ":19")
+}
